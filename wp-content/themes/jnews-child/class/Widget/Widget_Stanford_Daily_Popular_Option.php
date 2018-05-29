d55a0468f34a89dc8c8c7529ea6c13070dfc1dc6
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-05-29 09:55:08 -0700

    add views and option for widget

---
+++ wp-content/themes/jnews-child/class/Widget/Widget_Stanford_Daily_Popular_Option.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author : Jegtheme
+ */
+namespace JNews\Module\Widget;
+
+Class Widget_Stanford_Daily_Popular_Option extends WidgetOptionAbstract
+{
+    public function get_module_name()
+    {
+        return esc_html__('Stanford Daily - Popular Widget', 'jnews');
+    }
+}
---
+++ wp-content/themes/jnews-child/class/Widget/Widget_Stanford_Daily_Popular_View.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author : Jegtheme
+ */
+namespace JNews\Module\Widget;
+
+Class Widget_Stanford_Daily_Popular_View extends WidgetViewAbstract
+{
+}
