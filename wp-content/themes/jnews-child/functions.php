b3c35525ad57b528e1830fc4334a6bc8bf8f0682
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-07-29 17:11:39 -0400

    Show co-authors #19

--- wp-content/themes/jnews-child/functions.php
+++ wp-content/themes/jnews-child/functions.php
@@ -12,3 +12,19 @@ function jnews_child_enqueue_parent_style()
 {
     wp_enqueue_style( 'jnews-parent-style', get_parent_theme_file_uri('/style.css'));
 }
+
+
+/*
+ * Issue #19 - show co-authors.
+ */
+function jnews_the_author_link($author = null)
+{
+    if ( function_exists( 'coauthors_posts_links' ) ) {
+        return coauthors_post_links();
+    }
+    else {
+        printf('<a href="%1$s">%2$s</a>',
+            esc_url( get_author_posts_url( get_the_author_meta('ID', $author) ) ),
+            get_the_author_meta('display_name', $author));
+    }
+}
