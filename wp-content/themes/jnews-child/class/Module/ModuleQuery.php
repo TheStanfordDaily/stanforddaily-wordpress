a8ff5ab89eacff709fb74a519b8303ea3513456a
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-10-09 21:44:02 -0700

    merge posts with tsd_include_author_or

--- wp-content/themes/jnews-child/class/Module/ModuleQuery.php
+++ wp-content/themes/jnews-child/class/Module/ModuleQuery.php
@@ -380,9 +380,23 @@ Class ModuleQuery
             $args['date_query'] = $attr['date_query'];
         }
 
-        // Query
-        $query = new \WP_Query( $args );
 
+
+        if ( isset( $attr['tsd_include_author_or'] ) ) 
+        {
+            $args["include_author"] = $attr['tsd_include_author_or'];
+            unset($args["tax_query"]);
+            $query2 = new \WP_Query( $args );
+            $query1 = new \WP_Query( $args );
+            $query = new \WP_Query();
+            $query->posts = array_merge( $query1->posts, $query2->posts );
+            //populate post_count count for the loop to work correctly
+            $query->post_count = $query1->post_count + $query2->post_count;
+        }
+        else {
+            // Query
+            $query = new \WP_Query( $args );
+        }
 
         foreach($query->posts as $post) {
             $result[] = $post;
