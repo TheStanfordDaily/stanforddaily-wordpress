21aedb92184ded8afb8cb1e0e86383a84c4a423e
Yifei He <yifei@hesyifei.com>
2019-01-07 11:03:44 -0800

    [TSD Theme] add author page & rename content-search.php to content-archive.php

--- wp-content/themes/thestanforddaily/search.php
+++ wp-content/themes/thestanforddaily/search.php
@@ -32,9 +32,9 @@ get_header();
 				/**
 				 * Run the loop for the search to output the results.
 				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
+				 * called content-archive.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content', 'archive' );
 
 			endwhile;
 
