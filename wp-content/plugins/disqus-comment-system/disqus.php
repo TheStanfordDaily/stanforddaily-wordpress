05d04fb1db36191c16cdca3a7b1d2ac3c24d43c4
Yifei He <yifei@hesyifei.com>
2019-01-18 21:55:51 -0800

    install official Disqus for WordPress plugin instead

--- wp-content/plugins/disqus-comment-system/disqus.php
+++ wp-content/plugins/disqus-comment-system/disqus.php
@@ -12,6 +12,7 @@
  * @package           Disqus
  *
  * @wordpress-plugin
+ * Plugin Name:       Disqus for WordPress
  * Plugin URI:        https://disqus.com/
  * Description:       Disqus helps publishers increase engagement and build loyal audiences. Supports syncing comments to your database for easy backup.
  * Version:           3.0.16
