c6fbdcc52a9ada236beac9456c82f01167e2d54d
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-03 14:46:48 -0800

    sidebar: try another jquery

--- wp-content/themes/jnews-child/sidebar-tips.js
+++ wp-content/themes/jnews-child/sidebar-tips.js
@@ -1,4 +1,4 @@
-$(function() {
+jQuery(function() {
     $("<div class='tsd-sidebar-tips-container'><h3>Confidential Tips</h3>" +
     "<input type='text' placeholder='name'><input type='text' placeholder='email (optional)'>" +
     "<textarea placeholder='message'></textarea><button type='submit'>Submit</button>" +
