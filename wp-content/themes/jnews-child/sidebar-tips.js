ca23705f8c9252c29a059ceb067b6ebcb2f522a2
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-05 14:55:09 -0800

    fix: move tips widget to the top navbar

--- wp-content/themes/jnews-child/sidebar-tips.js
+++ wp-content/themes/jnews-child/sidebar-tips.js
@@ -1,4 +1,7 @@
 jQuery(function() {
-    jQuery(jQuery("#tsd-tips-widget").html()).prependTo(jQuery('.jeg_sidebar'));
+    // jQuery(jQuery("#tsd-tips-widget").html()).prependTo(jQuery('.jeg_sidebar'));
+    jQuery(".jeg_bottombar .jeg_nav_normal").last().replaceWith(
+        jQuery(jQuery("#tsd-tips-widget").html())
+    );
     jQuery(jQuery("#tsd-donate-header").html()).prependTo(jQuery('.jeg_viewport'));
 });
