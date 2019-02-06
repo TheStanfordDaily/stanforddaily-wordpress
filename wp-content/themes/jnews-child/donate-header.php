057895982b7de457f68f5c4fb3d384af07f587c1
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-06 14:27:22 -0800

    fix: fix center align of header

--- wp-content/themes/jnews-child/donate-header.php
+++ wp-content/themes/jnews-child/donate-header.php
@@ -19,7 +19,7 @@
             Support independent, student-run journalism.
         </mark>
     </h3>
-    <div class="tsd-hidden-sm">
+    <div class="tsd-hidden-sm" style="text-align: left;">
     <i class="fa fa-book fa-4x" aria-hidden="true" style="float: left; margin: 5px 30px 0px 30px;"></i>
     Your support helps give staff members from all backgrounds the opportunity to conduct meaningful reporting on important issues at Stanford. All contributions are tax-deductible.
     </div>
