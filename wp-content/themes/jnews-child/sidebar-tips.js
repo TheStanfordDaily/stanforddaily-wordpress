63337a12868e5dd77932565df2ad47ef69b85f95
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-05 21:05:29 -0800

    feat: add tips widget with google sheets backend

--- wp-content/themes/jnews-child/sidebar-tips.js
+++ wp-content/themes/jnews-child/sidebar-tips.js
@@ -14,6 +14,31 @@
                 recurringForm.submit();
                 return false;
             }
-        })
+        });
+
+        $("a.tsd-tips-toggle").click(function(e) {
+            e.preventDefault();
+            var expandedElement = $(this).next("tsd-tips-expanded");
+            var icon = $(this).find("i.fa");
+            expandedElement.slideToggle(400, function() {
+                if (expandedElement.is(":visible")) {
+                    icon.removeClass("fa-caret-down").addClass("fa-caret-up");
+                }
+                else {
+                    icon.addClass("fa-caret-down").removeClass("fa-caret-up");
+                }
+            })
+        });
+
+        $(".tsd-tips-expanded form").submit(function(e) {
+            $.post($(this).attr("action"), $(this).serialize())  .done(function() {
+                alert( "Tip submitted successfully. Thank you!" );
+                $("a.tsd-tips-toggle").click();
+              })
+              .fail(function() {
+                alert( "Error submitting the form. Please email your tip to eic@stanforddaily.com." );
+              });
+            return false;
+        });
     });
 })(jQuery);
