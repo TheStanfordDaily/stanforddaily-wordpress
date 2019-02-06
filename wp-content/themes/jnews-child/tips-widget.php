51fa43157079fa653f5b1a1ece09c97870a79ce5
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-05 21:27:48 -0800

    fix: fix tips expansion

--- wp-content/themes/jnews-child/tips-widget.php
+++ wp-content/themes/jnews-child/tips-widget.php
@@ -4,15 +4,15 @@
     <i class="fa fa-caret-down" style="
         margin-left: 10px;
     "></i></div>
-    <div class="tsd-tips-expanded">
-        <div class="tsd-sidebar-tips-container">
-            <h3>Submit a Tip</h3>
-            <form action="https://script.google.com/macros/s/AKfycbzTLxlB1iYpJh8gEbeHEXEGiZiWOoglns3NJJf3YzrYLzn-W5Q/exec" method="POST">
-                <textarea name="message" placeholder="message" style="line-height: initial;"></textarea>
-                <input type="text" name="name" placeholder="name (optional)" />
-                <input type="email" name="email" placeholder="email (optional)" />
-                <input type="submit" />
-            </form>
-        </div>
-    </div>
 </a>
+<div class="tsd-tips-expanded">
+    <div class="tsd-sidebar-tips-container">
+        <h3>Submit a Tip</h3>
+        <form action="https://script.google.com/macros/s/AKfycbzTLxlB1iYpJh8gEbeHEXEGiZiWOoglns3NJJf3YzrYLzn-W5Q/exec" method="POST">
+            <textarea name="message" placeholder="message" style="line-height: initial;"></textarea>
+            <input type="text" name="name" placeholder="name (optional)" />
+            <input type="email" name="email" placeholder="email (optional)" />
+            <input type="submit" />
+        </form>
+    </div>
+</div>
