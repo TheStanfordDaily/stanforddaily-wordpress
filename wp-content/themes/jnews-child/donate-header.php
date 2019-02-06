5c7cdb6f9b65563a97f2ac1baec2c6c33ce68e9b
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-05 20:02:39 -0800

    fix: make donation blurb smaller

--- wp-content/themes/jnews-child/donate-header.php
+++ wp-content/themes/jnews-child/donate-header.php
@@ -1,18 +1,21 @@
-<div style="
+<div class="row" style="
     background: #eee;
     padding: 20px;
     text-align: center;
     border-top: 1px solid #aa0000;
 ">
+
+<div class="col-xs-12 col-sm-6">
     <h3 style="
     /* padding: 0; */
-    margin-top: 2px;
-">Support independent, student-run journalism</h3>
-<i class="fa fa-book fa-4x" aria-hidden="true" style="float: right"></i>
-<p>
-We're a student-run organization committed to providing hands-on experience in journalism, digital media and business for the next generation of reporters.
-Your support makes a difference in helping give staff members from all backgrounds the opportunity to develop important professional skills and conduct meaningful reporting. All contributions are tax-deductible.
-    </p>
+    margin-top: 0px;
+    text-align: center;
+    ">Support independent, student-run journalism</h3>
+    <i class="fa fa-book fa-4x" aria-hidden="true" style="float: left; margin: 5px 30px 0px 30px;"></i>
+    Your support makes a difference in helping give staff members from all backgrounds the opportunity to conduct meaningful reporting. All contributions are tax-deductible.
+</div>
+
+<div class="col-xs-12 col-sm-6">
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" class="customize-unpreviewable">
     <input type="hidden" name="business" value="coo@stanforddaily.com">
     <input type="hidden" name="cmd" value="_donations">
@@ -33,9 +36,21 @@ echo home_url($wp->request);?>">
         <option>$1,000</option>
     </select>
 
+    <div class="checkbox" style="
+    /* display: inline-block; */
+    margin: 0px 10px;
+    margin-top: 10px;
+    " name="monthly-donation">
+    <label><input type="checkbox" value="" style="
+        margin-right: 10px;
+    ">Make my donation a monthly donation.</label>
+    </div>
+
     <!-- Display the payment button. -->
 
     <img alt="" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">
+</div>
+
 
 </form>
 </div>
