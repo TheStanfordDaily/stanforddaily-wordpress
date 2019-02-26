2ffc04f50f06275890886b5edc5718c3d41cbbcd
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-25 18:03:39 -0800

    feat: add titles and sports section

--- wp-content/themes/thestanforddaily/home.php
+++ wp-content/themes/thestanforddaily/home.php
@@ -48,6 +48,27 @@ get_header();
 					wp_reset_query();
 					?>
 				</div>
+				<div class="row tsd-block-title">
+					<div class="col-12">
+						<h3 class="tsd-block-title">Sports</h3>
+					</div>
+				</div>
+				<div class="row mt-4">
+					<?php
+					query_posts( array ( 'category_name' => 'SPORTS', 'posts_per_page' => 6 ) );
+					if ( have_posts() ) :
+						while ( have_posts() ) :
+							the_post();
+							echo '<div class="col-12 col-sm-6 tsd-excerpt-container-small">';
+							get_template_part( 'template-parts/excerpt', get_post_type() );
+							echo "</div>";
+						endwhile;
+					else:
+						get_template_part( 'template-parts/content', 'none' );
+					endif;
+					wp_reset_query();
+					?>
+				</div>
 			</div>
 			
 
