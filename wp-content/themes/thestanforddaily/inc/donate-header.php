2bd039a047b16d8152c4be4bf0469cc713a11a64
Yifei He <yifei@hesyifei.com>
2019-03-01 10:06:08 -0800

    [TSD Theme] minor changes

--- wp-content/themes/thestanforddaily/inc/donate-header.php
+++ wp-content/themes/thestanforddaily/inc/donate-header.php
@@ -2,26 +2,22 @@
 <div class="row tsd-donate-header">
 
 <a class="tsd-donate-header-close" href="#" >
-    <i class="fa fa-close"></i>
+    <i class="fas fa-times"></i>
 </a>
 
-<div class="col-12 col-sm-6">
-    <h3 style="
-    /* padding: 0; */
-    margin-top: 0px;
-    text-align: center;
-    ">
+<div class="col-12 col-lg-8">
+    <h3 style="margin-top: 0px; text-align: center;">
         <mark style="color: black">
             Support independent, student-run journalism.
         </mark>
     </h3>
     <div class="tsd-hidden-sm" style="text-align: left;">
-    <i class="fa fa-book fa-4x" aria-hidden="true" style="float: left; margin: 5px 30px 0px 30px;"></i>
+    <i class="fas fa-newspaper fa-4x" aria-hidden="true" style="float: left; margin: 5px 30px 0px 30px;"></i>
     Your support helps give staff members from all backgrounds the opportunity to conduct meaningful reporting on important issues at Stanford. All contributions are tax-deductible.
     </div>
 </div>
 
-<div class="col-12 col-sm-6">
+<div class="col-12 col-lg-4">
     <?php $donation_location = "header"; include "donation-form.php"; ?>
 </div>
 </div>
