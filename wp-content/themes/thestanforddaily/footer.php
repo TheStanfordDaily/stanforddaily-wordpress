1bb4d978bb8c946754a25480acb7d931e461893a
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-03-01 15:58:43 -0800

    feat: add privacy policy and mobile app links to footer

--- wp-content/themes/thestanforddaily/footer.php
+++ wp-content/themes/thestanforddaily/footer.php
@@ -15,14 +15,21 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="container">
-			<div class="site-info">
-				&copy; <?php echo date("Y"); ?> The Stanford Daily Publishing Corporation.
-				<br />
-				Proudly powered by <a href="https://wordpress.org/" target="_blank">WordPress</a>
-				<span class="sep"> | </span>
-				Theme by <a href="/wp-content/themes/thestanforddaily/about.html" target="_blank">TSD Tech Team</a>
-			</div><!-- .site-info -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="site-info col-6">
+					&copy; <?php echo date("Y"); ?> The Stanford Daily Publishing Corporation.
+					<br />
+					Proudly powered by <a href="https://wordpress.org/" target="_blank">WordPress</a>
+					<span class="sep"> | </span>
+					Theme by <a href="/wp-content/themes/thestanforddaily/about.html" target="_blank">TSD Tech Team</a>
+				</div><!-- .site-info -->
+				<div class="col-6" style="text-align: right; padding-right: 25px;">
+					<a href="/privacy-policy">Privacy Policy</a>
+					<span class="sep"> | </span>
+					<a href="/mobile-app/">Mobile App</a>
+				</div>
+			</div>
 		</div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
