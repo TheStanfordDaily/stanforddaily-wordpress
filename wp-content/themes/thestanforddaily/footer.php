51b271a37a535697cc2b8457c3053064386b74ec
Yifei He <yifei@hesyifei.com>
2019-03-01 16:16:56 -0800

    [TSD Theme] better footer design

--- wp-content/themes/thestanforddaily/footer.php
+++ wp-content/themes/thestanforddaily/footer.php
@@ -17,17 +17,17 @@
 	<footer id="colophon" class="site-footer">
 		<div class="container">
 			<div class="row">
-				<div class="site-info col-6">
+				<div class="site-info site-info-left col-12 col-lg-6">
 					&copy; <?php echo date("Y"); ?> The Stanford Daily Publishing Corporation.
 					<br />
 					Proudly powered by <a href="https://wordpress.org/" target="_blank">WordPress</a>
 					<span class="sep"> | </span>
 					Theme by <a href="/wp-content/themes/thestanforddaily/about.html" target="_blank">TSD Tech Team</a>
 				</div><!-- .site-info -->
-				<div class="col-6" style="text-align: right; padding-right: 25px;">
-					<a href="/privacy-policy">Privacy Policy</a>
+				<div class="site-info site-info-right col-12 col-lg-6">
+					<a href="/privacy-policy/" target="_blank">Privacy Policy</a>
 					<span class="sep"> | </span>
-					<a href="/mobile-app/">Mobile App</a>
+					<a href="/mobile-app/" target="_blank">Mobile App</a>
 				</div>
 			</div>
 		</div><!-- .container -->
