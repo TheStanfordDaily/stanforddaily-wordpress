6ca0a86c6df8e5728567587dd832ed86d85f3bed
Yifei He <yifei@hesyifei.com>
2019-03-06 16:45:24 -0800

    [TSD Theme] fix #138

--- wp-content/themes/thestanforddaily/functions.php
+++ wp-content/themes/thestanforddaily/functions.php
@@ -123,7 +123,7 @@ function tsd_scripts() {
 	//wp_enqueue_style( 'tsd-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'tsd-style', get_stylesheet_directory_uri() . "/style.min.css" );
 	wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css' );
-	wp_enqueue_style( 'load-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Libre+Baskerville:400,400i,700' );
+	wp_enqueue_style( 'load-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Libre+Baskerville|PT+Serif:400,400i,700' );
 	wp_enqueue_style( 'bootstrap-grid', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap-grid.min.css' );
 
 	wp_enqueue_script( 'jquery' );
