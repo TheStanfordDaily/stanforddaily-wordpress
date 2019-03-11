7085f7f2f03aa301eaf51ac9bea9222790796da9
Yifei He <yifei@hesyifei.com>
2019-03-10 22:10:03 -0700

    [PN Plugin] support basic sending notification for post on publish

--- wp-content/plugins/tsd-push-notification/inc/auto-send-subscription.php
+++ wp-content/plugins/tsd-push-notification/inc/auto-send-subscription.php
@@ -1,1 +1,28 @@
 <?php
+// https://wordpress.stackexchange.com/a/88335/75147
+function tsd_pn_post_transition_post_status( $new_status, $old_status, $post ) {
+	if ( $new_status == 'publish' && $old_status == 'new' ) {
+		// the post is inserted
+	} else if ( $new_status == 'publish' && $old_status != 'publish' ) {
+		// the post is published
+		tsd_pn_post_send_new_notification( $post );
+	} else {
+		// the post is updated
+	}
+}
+add_action( 'transition_post_status', 'tsd_pn_post_transition_post_status', 10, 3 );
+
+function tsd_pn_post_send_new_notification( $post ) {
+	$notification_body = trim( strip_tags( get_extended( $post->post_content )[ "main" ] ) );
+
+	$notification_data = [
+		"post_id" => $post->ID,
+	];
+
+	$send_results = tsd_send_expo_push_notification(
+		[1144875, 1144876],	// TODO: actually get receivers
+		$post->post_title,
+		$notification_body,
+		$notification_data
+	);
+}
