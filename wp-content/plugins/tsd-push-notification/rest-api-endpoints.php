4e9cf48e1f8b585ed9bec0b19c7ae50405ebcd8d
Yifei He <yifei@hesyifei.com>
2019-03-10 19:32:12 -0700

    [PN Plugin] minor change

--- wp-content/plugins/tsd-push-notification/rest-api-endpoints.php
+++ wp-content/plugins/tsd-push-notification/rest-api-endpoints.php
@@ -1,79 +1,79 @@
 <?php
 // Custom WP API endpoint
 function tsd_push_notification_enable_api() {
-    // Ref: https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
+	// Ref: https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
 
-    // Create json-api endpoint
-    add_action('rest_api_init', function () {
-        // Match "/push-notification/users/{token}"
-        // Ref: https://restfulapi.net/rest-put-vs-post/
-        register_rest_route('tsd/v1', '/push-notification/users/(?P<token>(.+))', [
-            'methods' => 'PUT',
-            'callback' => 'tsd_push_notification_update_user',
-            'permission_callback' => function (WP_REST_Request $request) {
-                return true;
-            }
-        ]);
-    });
+	// Create json-api endpoint
+	add_action('rest_api_init', function () {
+		// Match "/push-notification/users/{token}"
+		// Ref: https://restfulapi.net/rest-put-vs-post/
+		register_rest_route('tsd/v1', '/push-notification/users/(?P<token>(.+))', [
+			'methods' => 'PUT',
+			'callback' => 'tsd_push_notification_update_user',
+			'permission_callback' => function (WP_REST_Request $request) {
+				return true;
+			}
+		]);
+	});
 
-    function tsd_push_notification_update_user( $request ) {
-        $user_token = $request[ "token" ];
-        $user_subscribing = $request[ "subscribing" ];
+	function tsd_push_notification_update_user( $request ) {
+		$user_token = $request[ "token" ];
+		$user_subscribing = $request[ "subscribing" ];
 
-        // Change e.g. "breaking" to 1.
-        $list_ids = [];
-        foreach ( $user_subscribing[ "list" ] as $each_list_name ) {
-            $list_ids[] = tsd_pn_get_sub_list_id_from_name( $each_list_name );
-        }
-        $user_subscribing[ "list" ] = $list_ids;
+		// Change e.g. "breaking" to 1.
+		$list_ids = [];
+		foreach ( $user_subscribing[ "list" ] as $each_list_name ) {
+			$list_ids[] = tsd_pn_get_sub_list_id_from_name( $each_list_name );
+		}
+		$user_subscribing[ "list" ] = $list_ids;
 
-        $this_user = get_posts( [
-            'post_type' => 'tsd_pn_receiver',
-            'name' => $user_token,
-        ] );
-        $post_id = -1;
-        if ( empty( $this_user ) ) {
-            $post_id = tsd_create_new_pn_receiver( $user_token, $user_subscribing[ "category_ids" ], $user_subscribing[ "author_ids" ], $user_subscribing[ "location_ids" ] );
-        } else if ( count( $this_user ) == 1 ) {
-            $post_id = $this_user[0]->ID;
-            tsd_create_update_pn_receiver( $post_id, $user_subscribing[ "category_ids" ], $user_subscribing[ "author_ids" ], $user_subscribing[ "location_ids" ] );
-        } else {
-            echo "WARNING: SHOULD NOT HAVE MORE THAN 1 POST WITH SAME NAME";
-            return [];  // TODO: return error;
-        }
+		$this_user = get_posts( [
+			'post_type' => 'tsd_pn_receiver',
+			'name' => $user_token,
+		] );
+		$post_id = -1;
+		if ( empty( $this_user ) ) {
+			$post_id = tsd_create_new_pn_receiver( $user_token, $user_subscribing[ "category_ids" ], $user_subscribing[ "author_ids" ], $user_subscribing[ "location_ids" ] );
+		} else if ( count( $this_user ) == 1 ) {
+			$post_id = $this_user[0]->ID;
+			tsd_create_update_pn_receiver( $post_id, $user_subscribing[ "category_ids" ], $user_subscribing[ "author_ids" ], $user_subscribing[ "location_ids" ] );
+		} else {
+			echo "WARNING: SHOULD NOT HAVE MORE THAN 1 POST WITH SAME NAME";
+			return [];  // TODO: return error;
+		}
 
-        if ( $post_id != -1 ) {
-            foreach ( tsd_pn_get_subscription_types() as $each_type ) {
-                tsd_pn_sub_update_subscription( $post_id, $each_type, $user_subscribing[ $each_type ] );
-            }
-        }
+		if ( $post_id != -1 ) {
+			foreach ( tsd_pn_get_subscription_types() as $each_type ) {
+				tsd_pn_sub_update_subscription( $post_id, $each_type, $user_subscribing[ $each_type ] );
+			}
+		}
 
-        // TODO: return success / error message
-        return [];
-    }
+		// TODO: return success / error message
+		return [];
+	}
 
-    // TODO: Find better way to store category_ids, author_ids, location_ids.
+	// TODO: Find better way to store category_ids, author_ids, location_ids.
 
-    function tsd_create_new_pn_receiver( $token, $category_ids = [], $author_ids = [], $location_ids = [] ) {
-        // insert the post and set the category
-        $post_id = wp_insert_post([
-            'post_type' => 'tsd_pn_receiver',
-            'post_title' => $token,
-            'post_status' => "publish",
-            /*'meta_input' => [
-                "category_ids" => json_encode( $category_ids ),
-                "author_ids" => json_encode( $author_ids ),
-                "location_ids" => json_encode( $location_ids ),
-            ]*/
-        ]);
-        return $post_id;
-    }
+	function tsd_create_new_pn_receiver( $token, $category_ids = [], $author_ids = [], $location_ids = [] ) {
+		// insert the post and set the category
+		$post_id = wp_insert_post([
+			'post_type' => 'tsd_pn_receiver',
+			'post_title' => $token,
+			'post_status' => "publish",
+			/*'meta_input' => [
+				"category_ids" => json_encode( $category_ids ),
+				"author_ids" => json_encode( $author_ids ),
+				"location_ids" => json_encode( $location_ids ),
+			]*/
+		]);
+		return $post_id;
+	}
 
-    function tsd_create_update_pn_receiver( $post_id, $category_ids, $author_ids, $location_ids ) {
-        // Do nothing.
-        /*update_post_meta( $post_id, 'category_ids', json_encode( $category_ids ) );
-        update_post_meta( $post_id, 'author_ids', json_encode( $author_ids ) );
-        update_post_meta( $post_id, 'location_ids', json_encode( $location_ids ) );*/
-    }
+	function tsd_create_update_pn_receiver( $post_id, $category_ids, $author_ids, $location_ids ) {
+		// Do nothing.
+		/*update_post_meta( $post_id, 'category_ids', json_encode( $category_ids ) );
+		update_post_meta( $post_id, 'author_ids', json_encode( $author_ids ) );
+		update_post_meta( $post_id, 'location_ids', json_encode( $location_ids ) );*/
+	}
 }
 add_action('init', 'tsd_push_notification_enable_api');
