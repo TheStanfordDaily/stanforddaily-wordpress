68594ff3ddabd0c8661699641ecf973a40c2ceb0
Yifei He <yifei@hesyifei.com>
2019-03-20 18:25:38 -0700

    [TSD Theme] minor change

--- wp-content/themes/thestanforddaily/inc/tips-widget-mobile.php
+++ wp-content/themes/thestanforddaily/inc/tips-widget-mobile.php
@@ -1,8 +1,8 @@
 <a href="#" class="tsd-tips-toggle tsd-tips-toggle-mobile">
 	TIPS
-	<i class="fa fa-caret-down" style=""></i>
+	<i class="fa fa-caret-down"></i>
 </a>
-<div class="tsd-tips-expanded">
+<div class="tsd-tips-expanded" style="top: 60px;">
 	<div class="tsd-sidebar-tips-container">
 		<h3>Submit a Tip</h3>
 		<form action="#" method="POST">
