813dcf6659b222d157bbd55e4f65b7f472db756f
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-14 21:53:34 -0700

    feat: add link rel=preload

--- wp-content/themes/thestanforddaily/functions.php
+++ wp-content/themes/thestanforddaily/functions.php
@@ -212,3 +212,15 @@ $image_size = [$prefix . '1140x570' => array('width' => 1140, 'height' => 570, '
 foreach($image_size as $id => $image) {
 	add_image_size( $id, $image['width'], $image['height'], $image['crop'] );
 }
+
+function add_rel_preload($html, $handle, $href, $media) {
+    
+    if (is_admin())
+        return $html;
+
+     $html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+EOT;
+    return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
