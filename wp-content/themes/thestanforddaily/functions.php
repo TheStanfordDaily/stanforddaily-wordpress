eb59de93f279ee0a53aa22b3c3e08f3b1ac04a49
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-21 21:05:14 -0700

    Revert "feat: add script async"
    This reverts commit 53ecbc616c68ca659753222556ccb5e00f170fce.

--- wp-content/themes/thestanforddaily/functions.php
+++ wp-content/themes/thestanforddaily/functions.php
@@ -224,11 +224,3 @@ EOT;
     return $html;
 }
 add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
-
-// Make JavaScript Asynchronous in Wordpress
-add_filter( 'script_loader_tag', function ( $tag, $handle ) {    
-    if( is_admin() ) {
-        return $tag;
-    }
-    return str_replace( ' src', ' async src', $tag );
-}, 10, 2 );
