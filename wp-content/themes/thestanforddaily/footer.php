9c3eb538d974d46da2e882080c85576f7443edb8
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-21 21:38:15 -0700

    fix: update mobile app link

--- wp-content/themes/thestanforddaily/footer.php
+++ wp-content/themes/thestanforddaily/footer.php
@@ -27,7 +27,7 @@
 				<div class="site-info site-info-right col-12 col-lg-6">
 					<a href="/privacy-policy/" target="_blank">Privacy Policy</a>
 					<span class="sep"> | </span>
-					<a href="/mobile-app/" target="_blank">Mobile App</a>
+					<a href="https://app.stanforddaily.com" target="_blank">Mobile App</a>
 				</div>
 			</div>
 		</div><!-- .container -->
