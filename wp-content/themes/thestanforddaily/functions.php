df6c98d484bd3d0e878abbedaeb862502df5f8be
Yifei He <yifei@hesyifei.com>
2019-04-23 22:57:35 -0700

    [TSD Theme] add comments and todo for add_rel_preload

--- wp-content/themes/thestanforddaily/functions.php
+++ wp-content/themes/thestanforddaily/functions.php
@@ -213,14 +213,15 @@ foreach($image_size as $id => $image) {
 	add_image_size( $id, $image['width'], $image['height'], $image['crop'] );
 }
 
+// Ref: https://stackoverflow.com/a/54816082/2603230
+// TODO: Broken on FireFox.
 function add_rel_preload($html, $handle, $href, $media) {
-    
-    if (is_admin())
-        return $html;
+	if (is_admin())
+		return $html;
 
-     $html = <<<EOT
+	$html = <<<EOT
 <link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
 EOT;
-    return $html;
+	return $html;
 }
 add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
