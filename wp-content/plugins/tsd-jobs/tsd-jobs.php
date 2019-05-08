5019062fb17b8ee16462da7308ee2e8b03c21259
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-08 14:55:41 -0700

    feat: add rest jobs controller

--- wp-content/plugins/tsd-jobs/tsd-jobs.php
+++ wp-content/plugins/tsd-jobs/tsd-jobs.php
@@ -2,6 +2,7 @@
 /**
 * Plugin Name: The Stanford Daily - Jobs
 * Description: Add jobs endpoint onto REST API, which can be consumed by our jobs site.
+* See http://localhost.stanforddaily.com/wp-json/tsd/v1/jobs
 * Author: The Stanford Daily Tech Team
 * Version: 1.0
 * History:
@@ -10,12 +11,14 @@
 
 
 function tsd_jobs_plugin_enable_api() {
+  include "inc/tsd-rest-jobs-controller.php";
     $args = array(
       'public'       => true,
       'show_in_rest' => true,
       'rest_base' => 'jobs',
       'label'        => 'Jobs',
-      'show_in_menu' => true
+      'show_in_menu' => true,
+      'rest_controller_class' => 'TSD_REST_Jobs_Controller'
     );
     register_post_type( 'job', $args );
 }
