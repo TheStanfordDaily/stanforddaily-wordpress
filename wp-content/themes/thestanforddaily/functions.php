e254b7e68b7c40729825b332f84156df2f3a4712
Grace <gkzhou@stanford.edu>
2019-05-23 21:19:51 -0700

    Shortens manual excerpts

--- wp-content/themes/thestanforddaily/functions.php
+++ wp-content/themes/thestanforddaily/functions.php
@@ -149,6 +149,29 @@ function tsd_scripts() {
 add_action( 'wp_enqueue_scripts', 'tsd_scripts' );
 
 /**
+ * Creates an excerpt if needed; and shortens the manual excerpt as well
+ */
+function wp_trim_all_excerpt($text) {
+	global $post;
+	  $raw_excerpt = $text;
+	  if ( '' == $text ) {
+		$text = get_the_content('');
+		$text = strip_shortcodes( $text );
+		$text = apply_filters('the_content', $text);
+		$text = str_replace(']]>', ']]&gt;', $text);
+	  }
+	 
+	$text = strip_tags($text);
+	$excerpt_length = apply_filters('excerpt_length', 55);
+	$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+	$text = wp_trim_words( $text, $excerpt_length, $excerpt_more ); 
+	return apply_filters('wp_trim_excerpt', $text, $raw_excerpt); 
+}
+	 
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'wp_trim_all_excerpt');
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
