31b5bc3692879ee37d4495a47a8c9b7d17d7f1e2
Yifei He <yifei@hesyifei.com>
2019-10-05 22:12:09 -0700

    Add some functions support to `tsd-headless`

---
+++ wp-content/themes/tsd-headless/header.php
@@ -0,0 +1,9 @@
+<link rel="profile" href="https://gmpg.org/xfn/11">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+	(adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-8817523005861595",
+		enable_page_level_ads: true
+	});
+</script>
+<?php wp_head(); ?>
