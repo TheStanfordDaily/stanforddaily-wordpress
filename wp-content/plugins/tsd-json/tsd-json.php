aed33f18b3f13a34d94fe483310c153b5003943f
Holden Foreman <38192823+hs4man21@users.noreply.github.com>
2020-11-29 09:02:52 -0800

    Update tsd-json.php

--- wp-content/plugins/tsd-json/tsd-json.php
+++ wp-content/plugins/tsd-json/tsd-json.php
@@ -614,8 +614,8 @@ function tsd_json_plugin_enable_api() {
 
         // Create and configure a new client object.
         $client = new Google_Client();
-        $client->setApplicationName("Hello Analytics Reporting");
         return 1;
+        $client->setApplicationName("Hello Analytics Reporting");
         $client->setAuthConfig($KEY_FILE_LOCATION);
         $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
         $analytics = new Google_Service_AnalyticsReporting($client);
