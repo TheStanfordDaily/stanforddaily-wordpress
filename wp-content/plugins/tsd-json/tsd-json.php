f50aea89d1ef275c5184b6c6b908c3bba2e83e72
hs4man21 <hs4man21@stanford.edu>
2021-07-11 13:07:02 -0700

    Updated tsd json plugin to include custom fields in author object for author posts pages

--- wp-content/plugins/tsd-json/tsd-json.php
+++ wp-content/plugins/tsd-json/tsd-json.php
@@ -113,25 +113,6 @@ function tsd_json_plugin_enable_api() {
         return $authors;
     }
 
-    function tsd_json_plugin_get_indiv_author_info( $author, $include_author_description ) {
-        $image_url = "https://www.stanforddaily.com/wp-content/themes/thestanforddaily/img/placeholder-avatar.png";
-        $image_id = get_user_meta( $author->ID, "tsd_profileImage", true );
-        if ( ! empty( $image_id ) ) {
-            $image_url = wp_get_attachment_image_src( $image_id, 'thumbnail' )[0];
-        }
-        $new_author = [
-            'id' => $author->ID,
-            'displayName' => html_entity_decode( $author->display_name ),
-            'userNicename' => $author->user_nicename,
-            'url' => wp_make_link_relative( get_author_posts_url( $author->ID ) ),
-            'avatarUrl' => $image_url
-        ];
-        if ($include_author_description) {
-            $new_author[ 'description' ] = html_entity_decode( get_the_author_meta( 'description', $author->ID ) );
-        }
-        return $new_author;
-    }
-
     function copy_of_tsd_authors_plugin_author_info( $user ) {
         global $tsd_author_custom_fields;
         if ( $user === false ) {
@@ -600,7 +581,7 @@ function tsd_json_plugin_enable_api() {
 
         $head_and_footer = tsd_json_plugin_get_wp_head_and_wp_footer();
         $tsd_meta = $head_and_footer;
-        $tsd_meta[ "author" ] = tsd_json_plugin_get_indiv_author_info( $author, true );
+        $tsd_meta[ "author" ] = copy_of_tsd_authors_plugin_author_info( $author );
 
         return [
             "tsdMeta" => $tsd_meta,
